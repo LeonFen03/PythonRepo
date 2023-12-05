cc23f23793f6c7d19dc938a94b5efee343b0b0a4
LeonFen03 <LeonFen03@gmail.com>
2023-12-04 19:22:24 -0500

    Last solution

--- Activities/ppp-2.py
+++ Activities/ppp-2.py
@@ -18,3 +18,26 @@ def num_within(start,end,num):
         return True
     else: 
         return False
+
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    for row in triangle:
+      print(row)
